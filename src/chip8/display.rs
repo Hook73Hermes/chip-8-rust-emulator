09a734fcbdff4503ee4d85e739aae3e770cbcead
Hook73Hermes <l.maietti.fe@gmail.com>
2026-01-21 19:34:25 +0100

    Inizializzazione classi Display e Ram

---
+++ src/chip8/display.rs
@@ -0,0 +1,20 @@
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+pub const N_PIXELS: usize = SCREEN_WIDTH * SCREEN_HEIGHT;
+
+/* Display 64x32 con pixel bianchi (true) o neri (false) */
+pub struct Display {
+    pub buffer: [bool, N_PIXELS],
+}
+
+impl Display {
+    /* Inizializza tutti i pixel dello schermo spenti */
+    pub fn new() -> self {
+        buffer: [false, N_PIXELS],
+    }
+
+    /* Spegne tutti i pixel dello schermo */
+    pub fn clear(&mut self) {
+        self.buffer = [false, N_PIXELS];
+    } 
+}
---
+++ src/chip8/ram.rs
@@ -0,0 +1,25 @@
+const RAM_SIZE: usize = 4096;
+
+/* Ram con 4KB di memoria */
+pub struct Ram {
+    pub mem: [u8, RAM_SIZE],
+}
+
+impl Ram {
+    /* Inizializza tutta la ram a 0 */
+    pub fn new() -> self {
+        Ram {
+            mem: [0, RAM_SIZE],
+        }
+    }
+
+    /* Scrive un byte in memoria senza controllare che la locazione sia valida */
+    pub fn write_byte(&mut self, addr: u16, byte: u8) {
+        self.mem[addr as usize] = byte;
+    }
+
+    /* Legge u byte dalla memoria senza controllare che la locazione sia valida */
+    pub fn read_byte(&self, addr: u16) -> u8 {
+        self.mem[addr as usize]
+    }
+}
